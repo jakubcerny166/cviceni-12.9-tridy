34ac899ba539949807755fa827159c3b6551f8da
jakubcerny166 <cerny6@spsejecna.cz>
2022-09-12 08:16:40 +0200

    zmena

---
+++ cviceni 12.9 tridy/Interval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cviceni_12._9_tridy
+{
+    internal class Interval
+    {
+        private int horni;
+        private int dolni;
+
+        public int Horni
+        {
+            get { return horni; }
+            set
+            {
+                if(value < dolni)
+                {
+                    throw new Exception("horni nemuze byt mensi nez dolni");
+                }
+                horni = value; 
+            }
+        }
+
+    }
+}
